80200a969a72babd383baf84676c94d2492ba962
Walsh Fernandes <2016.walsh.fernandes@ves.ac.in>
2019-05-07 17:29:06 +0530

    Created module barcodescanner

---
+++ app/src/main/java/com/walshfernandes/barcodescannerexample/MainActivity.java
@@ -0,0 +1,48 @@
+package com.walshfernandes.barcodescannerexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+
+import com.google.android.gms.vision.barcode.Barcode;
+import com.walshfernandes.barcodescanner.BarcodeReader;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements BarcodeReader.BarcodeReaderListener {
+
+    BarcodeReader barcodeReader;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        barcodeReader = (BarcodeReader) getSupportFragmentManager().findFragmentById(R.id.barcode_scanner);
+    }
+
+    @Override
+    public void onScanned(Barcode barcode) {
+        Log.i("TAG", "Barcode text: " + barcode.displayValue);
+    }
+
+    @Override
+    public void onScannedMultiple(List<Barcode> barcodes) {
+
+    }
+
+    @Override
+    public void onBitmapScanned(SparseArray<Barcode> sparseArray) {
+
+    }
+
+    @Override
+    public void onScanError(String errorMessage) {
+
+    }
+
+    @Override
+    public void onCameraPermissionDenied() {
+
+    }
+}
